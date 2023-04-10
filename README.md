cafc1c7ab498fd6271fc5c6957d9758e7613045e
anthonyfrantz <tonesfrantz@gmail.com>
2023-04-10 13:37:27 +1000

    winning logic start

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Tic_Tac_Toe_2.0
-JavaScript 
+
+## JavaScript Version
+
+Build game in the DOM. (as opposed to HTML)
+
+**What do I need?**
+
+-   Ability to store input of players.
+-   Check for win without 16 loops
+-   Winning logic
+-   Switch player logic
+-   Reset
+-   Keep score over multiple games (Browser Memory)
+-   Click event
+-   Register which value was clicked
+-   Store value
+-   Check stored values for a win or not
+
+**Steps:**
+
+1. How to store input from players? Array, Object?
+2. render funtion() to read stored input and validate win or not. Make this have the bility to do 4\*4 or 585 games
+3. Once above 2 steps complete, create player turn funciton
+4. Store win over multiple games
+5. Build DOM game and assign values to interface to interact with logic. This will be where buttons and event listeners will be creaated to assign values previously tested.
--- main.js
+++ main.js
@@ -1,1 +1,120 @@
-console.log("HI I'm here");
+console.log("Hi, I'm here. This is my Tic Tac Toe Logic!");
+
+// // IDEA 1:
+const x = 'x';
+const o = 'o';
+const y = null;
+let gameSize = 3;
+let ticTacToe = {
+    playerInput: {},
+    createGame: function (gameSize) {
+        let i = gameSize * gameSize;
+        console.log(i);
+        while (i > 0) {
+            this.playerInput[i] = '-';
+            i--;
+        }
+    },
+    checkWin: function (playerInput) {
+        if ((gameSize = 3)) {
+            // Row 1
+            if (
+                this.playerInput[1] == this.playerInput[2] &&
+                this.playerInput[3] == this.playerInput[2]
+            ) {
+                console.log('WIN1');
+            }
+            // Row 2
+            else if (
+                this.playerInput[4] == this.playerInput[5] &&
+                this.playerInput[6] == this.playerInput[5]
+            ) {
+                console.log('WIN2');
+            }
+            //Row 3
+            else if (
+                this.playerInput[7] == this.playerInput[8] &&
+                this.playerInput[9] == this.playerInput[8]
+            ) {
+                console.log('WIN3');
+            }
+            // Diagonal Top left Bottom Left
+            else if (
+                this.playerInput[1] == this.playerInput[5] &&
+                this.playerInput[9] == this.playerInput[5]
+            ) {
+                console.log('WIN4');
+            }
+            // Diagonal Top Right
+            else if (
+                this.playerInput[3] == this.playerInput[5] &&
+                this.playerInput[7] == this.playerInput[5]
+            ) {
+                console.log('WIN5');
+            }
+            // Column 1
+            else if (
+                this.playerInput[1] == this.playerInput[4] &&
+                this.playerInput[7] == this.playerInput[4]
+            ) {
+                console.log('WIN6');
+            }
+            // Column 2
+            else if (
+                this.playerInput[2] == this.playerInput[5] &&
+                this.playerInput[8] == this.playerInput[5]
+            ) {
+                console.log('WIN7');
+            }
+            // Column 3
+            else if (
+                this.playerInput[3] == this.playerInput[6] &&
+                this.playerInput[9] == this.playerInput[6]
+            ) {
+                console.log('WIN8');
+            }
+        }
+    },
+};
+
+console.log(ticTacToe.createGame(gameSize));
+console.log(ticTacToe.playerInput);
+
+// checkWin: function (a) {
+//     let i = 9;
+//     if (this.playerInput[1] == this.playerInput[2]) {
+//         console.log(`Win`);
+//     }
+//     console.log(this.playerInput[1]);
+// while (i > 0) {
+//     if (
+//         (this.playerInput[1] == this.playerInput[2]) ==
+//         this.playerInput[3]
+//     ) {
+//         console.log('WIN');
+//     }
+//     console.log(`This is position ${i}: ${this.playerInput[i]}`);
+//     i--;
+// }
+
+// console.log(ticTacToe.checkWin(this.playerInput));
+// console.log(ticTacToe.playerInput);
+// console.log(ticTacToe.createGame(gameSize));
+
+// // IDEA 2:
+
+// let gameSize = 3;
+// let gameInput = [];
+
+// let createGame = function (gameSize) {
+//     for (i = 0; i < gameSize; i++) {
+//         gameInput[i] = gameInput.from(gameSize);
+//         console.log(gameInput);
+//         // for (j = 1; j < gameSize; j++) {
+//         //     gameInput[i].push('-');
+//         // }
+//     }
+// };
+
+// createGame(gameSize);
+// console.log(gameInput);
